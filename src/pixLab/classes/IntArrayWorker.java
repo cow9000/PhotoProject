554054345d945b7cf06d9fe5f00c8f8886ec6088
Derek Vawdrey <flyingpiechicken@gmail.com>
2018-01-22 12:12:28 -0700

    Added additional code from before winter break

--- src/pixLab/classes/IntArrayWorker.java
+++ src/pixLab/classes/IntArrayWorker.java
@@ -99,5 +99,36 @@ public class IntArrayWorker
       }
     }
   }
+  
+  
+  public int getCount(int i) {
+	  int numberOfTimes = 0;
+	  for(int row = 0; row < matrix.length; row++) {
+		  for(int col = 0; col < matrix[0].length; col++) {
+			  if(matrix[row][col] == i) {
+				  numberOfTimes += 1;
+			  }
+		  }
+	  }
+	  return numberOfTimes;
+  }
+  
+  public int getLargest() {
+	  int largestValue = 0;	
+	  for(int row = 0; row < matrix.length; row++) {
+		  for(int col = 0; col < matrix[0].length; col++) {
+			  if(matrix[row][col] > largestValue) largestValue = matrix[row][col];
+			}
+	  }
+	  return largestValue;
+  }
+  
+  public int getColTotal(int specificCol) {
+	  int total = 0;
+	  for(int row = 0; row < matrix.length; row++) {
+		  total += matrix[row][specificCol];
+	  }
+	  return total;
+  }
  
 }
